fc41fd02114f2c84f24bfc9757c7ce35608addcc
Domingo-Jhana Christina L.-BSIT 32E2 <148880134+Domingo-JhanaChristina-BSIT32E2@users.noreply.github.com>
2024-05-09 18:06:57 +0800

    222222

--- Midterm_Assignment1_Login/Midterm_Assignment1_Login/Models/ErrorViewModel.cs
+++ Midterm_Assignment1_Login/Midterm_Assignment1_Login/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Midterm_Assignment1_Login.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; } = "";
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
